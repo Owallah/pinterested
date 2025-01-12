7ec22a625086ad9cd274a7251b3f86b4484f421c
Owallah <mikesonowallah@gmail.com>
2025-01-13 00:27:57 +0300

    Associated pins with users and authenticated users

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_12_202918) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_12_210119) do
   create_table "pins", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
